71f1eec56afe5d1e20021371cbc87b9307f0da85
risa-s-92 <r-sato-1d8@eagle.sophia.ac.jp>
2021-07-04 20:54:56 +0900

    3,5,15の倍数

---
+++ 03_サンプル/01_3の倍数/main2.js
@@ -0,0 +1,14 @@
+// コンソールに1から24までの文字を表示
+const number = 24
+for (let n = 0; n < number + 1; n++) {
+  if (n % 15 === 0) {
+    console.log("FizzBuzz")
+  } else if (n % 3 === 0) {
+    console.log("Fizz")
+  } else if (n % 5 === 0) {
+    console.log("Buzz")
+  } else {
+    console.log(n)
+  }
+}
+// 数字を増やすにはどうしたらいいでしょう
---
+++ 04_ゼミ/week1/object.js
@@ -0,0 +1,15 @@
+const risa = {
+  name: "佐藤里咲",
+  furigana: "サトウリサ",
+  university: "Sophia University",
+  age: 20,
+  sayHello: function() {
+    alert("こんにちは")
+  },
+  hobby: ["ダンス", "音楽を聴く", "寝る"],
+  family: {
+    father: "エイジ",
+    mother: "リエコ",
+    brother: "リク",
+  },
+}
